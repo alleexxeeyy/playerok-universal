80918b27e1fb5e059bf7410a0023a41f31b1381c
alleexxeeyy <leshamen00@gmail.com>
2026-02-08 23:42:09 +0400

    1.7.6

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from colorama import Fore
 
-VERSION = "1.7.5"
+VERSION = "1.7.6"
 SKIP_UPDATES = False
 ACCENT_COLOR = Fore.LIGHTBLUE_EX
--- tgbot/templates/settings_comms.py
+++ tgbot/templates/settings_comms.py
@@ -37,13 +37,13 @@ def settings_comms_kb(page: int = 0):
 
     if total_pages > 1:
         buttons_row = []
-        btn_back = InlineKeyboardButton(text="←", callback_data=calls.CustomCustomCommandsPagination(page=page-1).pack()) if page > 0 else InlineKeyboardButton(text="🛑",callback_data="123")
+        btn_back = InlineKeyboardButton(text="←", callback_data=calls.CustomCommandsPagination(page=page-1).pack()) if page > 0 else InlineKeyboardButton(text="🛑",callback_data="123")
         buttons_row.append(btn_back)
             
         btn_pages = InlineKeyboardButton(text=f"{page+1}/{total_pages}",callback_data="enter_custom_commands_page")
         buttons_row.append(btn_pages)
         
-        btn_next = InlineKeyboardButton(text="→", callback_data=calls.CustomCustomCommandsPagination(page=page+1).pack()) if page < total_pages -1 else InlineKeyboardButton(text="🛑", callback_data="123")
+        btn_next = InlineKeyboardButton(text="→", callback_data=calls.CustomCommandsPagination(page=page+1).pack()) if page < total_pages -1 else InlineKeyboardButton(text="🛑", callback_data="123")
         buttons_row.append(btn_next)
         rows.append(buttons_row)
 
